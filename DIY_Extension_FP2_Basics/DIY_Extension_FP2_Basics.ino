9ff0421a8553a0b0640fcd58621214b3516d3c34
Christoph Kirschner <36758837+MisterK97@users.noreply.github.com>
2018-02-24 13:42:49 +0100

    Update Colorpicker => working now!!

--- DIY_Extension_FP2_Basics/DIY_Extension_FP2_Basics.ino
+++ DIY_Extension_FP2_Basics/DIY_Extension_FP2_Basics.ino
@@ -1,4 +1,5 @@
 ﻿#include <Adafruit_NeoPixel.h>
+
 #include <IRremote.h>
 
 //IR
@@ -10,13 +11,14 @@ const int sendingPin = 3;
 int LEDs[8]{ 15,14,13,12,11,10,9,8 };
 
 //Neopixel 
-#define PIN        17
-#define NUMPIXELS  1
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define PIN 17
+uint16_t NUMBER = 1;
+uint16_t PIXEL = 0;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMBER, PIN, NEO_GRB + NEO_KHZ800);
 
 //Serial
-long intRecieved = 0;
-byte buffer[4] = {0,0,0};
+byte buffer[4] = {0,0,0,0};
+int starttime = 0;
 
 //Temperature
 const long interval = 1000;           //interval of the temperaturechecks
@@ -25,8 +27,7 @@ const int ntc = A5;                   //analogRead-pin
 const int ntcNominal = 15000;         //resistance at e.g 25°C
 const int tempNominal = 25;           //nominaltemperature (e.g. 25°C)
 const int bCoefficient = 3528;        //Beta Coefficient(B25 from the datasheet of the NTC)
-const int serialResistor = 10000;     //value of the serial resistor
-									  
+const int serialResistor = 10000;     //value of the serial resistor									  
 int checks[checkingNumber];           //array variable for averaging the temperatur
 float averageValue = 0;               //variable for averaging the temperature
 float temp;                           //variable for calculation (Steinhart)
@@ -42,52 +43,73 @@ void setup()
 	for (int i = 0; i < 8; i++) {
 		pinMode(LEDs[i], OUTPUT);
 	}
+
  //NTC
-	pinMode(ntc, INPUT);            
+	pinMode(ntc, INPUT);
+
  //Neopixel
-	pixels.begin();
-	pinMode(17, OUTPUT);
+	strip.begin();
+	strip.setPixelColor(0, 255, 0, 0);
+	strip.show();
+	delay(1000);
+	strip.setPixelColor(0, 0, 0, 0);
+	strip.show();
 }
 
-void loop()
-{
+void loop() {
 	serialInterface();
+	control();
 	temperature();
+	sendTemp();
 }
 
-void serialInterface() {
+ void serialInterface() {
 	if (Serial.available() > 0) {
-		intRecieved = Serial.read();		
-		IntegerToBytes(intRecieved, buffer);
-		
-		switch (buffer[0]) {
-		// IR - Remote
-			case 1:
-		    IRremote();
-		    break;
-		//LED-Light
-			case 2:
-			lightLED();
-			break;
-		//RGB
-		    case 3:
-			neoPixel();
-			break;
-		}
-	}
-
- //send temperature to FP
-	int timeNow = millis();
-	if (timeNow - lastTempUpdate > 500) {
-		Serial.print(temp);
+		while (Serial.available() < 4 && ((millis() - starttime) < 1000)) {} // Wait 'till there are 4 Bytes waiting
+		for (int n = 0; n < 4; n++) { buffer[n] = Serial.read(); }		
 	}
 }
 
-void IntegerToBytes(long val, byte b[4]) {
-	b[0] = (byte)((val >> 24) & 0xff);
-	b[1] = (byte)((val >> 16) & 0xff);
-	b[2] = (byte)((val >> 8) & 0xff);
-	b[3] = (byte)(val & 0xff);
+ void sendTemp() {
+	 //send temperature to FP
+	 int timeNow = millis();
+	 if (timeNow - lastTempUpdate > 10000) {
+		 Serial.println((int)temp);
+		 lastTempUpdate = millis();
+	 }
+}
+
+void control() {
+	if (buffer[0] == 0) {
+		return;
+	}
+
+	//IRremote
+	if (buffer[0] == 1) {
+		//IRremote();
+		return;
+	}
+
+	//LEDs
+	if (buffer[0] == 2) {
+		lightLED();
+		return;
+	}
+
+	//RGB
+	if (buffer[0] != 3) {
+		return;
+	}
+
+	uint8_t r = buffer[1];
+	uint8_t g = buffer[2];
+	uint8_t b = buffer[3];
+	if (r < 20) { r = 0; };
+	if (g < 20) { g = 0; };
+	if (b < 20) { b = 0; };
+	strip.setPixelColor(PIXEL, r, g, b);
+	delay(10);
+	strip.show();
 }
 
 //lights up the 8 LEDs attached to the GPIOs
@@ -97,43 +119,28 @@ void lightLED() {
 	}
 }
 
-//lights up the Neopixel 
-void neoPixel() {
-	byte r = buffer[1];
-	byte g = buffer[2];
-	byte b = buffer[3];
-	pixels.setPixelColor(0, pixels.Color(r, g, b)); 
-	pixels.show(); 
-}
-
 //sends signal via IR-LED
 void IRremote() {
-	//sending selected hex-code
-	switch (buffer[2]) {
+	switch (buffer[1]) {
 	// ON
 	case 1:
-		irsend.sendNEC(0xFFB04F, 32);
-		Serial.println("ON");
+		irsend.sendNEC(0xFFB04F, 32); //sending selected hex-code
 		break;
 	// OFF
 	case 2:
 		irsend.sendNEC(0xFFF807, 32);
-		Serial.println("OFF");
 		break;
 	// RED
 	case 3:
 		irsend.sendNEC(0xFF9867, 32);
-		Serial.println("RED");
 		break;
 	// GREEN
 	case 4:
 		irsend.sendNEC(0xFFD827, 32);
-		Serial.println("GREEN");
 		break;
 	// BLUE
 	case 5:
 		irsend.sendNEC(0xFF8877, 32);
-		Serial.println("BLUE");
 		break;
 	}
 }
@@ -142,8 +149,7 @@ void IRremote() {
 void temperature() {
 	int timeNow = millis();
 	if (timeNow - lastTempCheck > 100) {
-		//N readouts with a short delay
-		for (int i = 0; i < checkingNumber; i++)
+		for (int i = 0; i < checkingNumber; i++) //N readouts with a short delay
 		{
 			checks[i] = analogRead(ntc);
 			delay(10);
@@ -151,8 +157,7 @@ void temperature() {
 
 //averaging of the measured values
 		averageValue = 0;
-		for (int i = 0; i < checkingNumber; i++)
-		{
+		for (int i = 0; i < checkingNumber; i++) {
 			averageValue += checks[i];
 		}
 		averageValue /= checkingNumber;
